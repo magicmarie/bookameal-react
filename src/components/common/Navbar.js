29b916d625be80915dc0be0354ea9c76d18a6a39
magicmarie <natukunda162@gmail.com>
2018-08-31 08:44:07 +0300

    Allow navbar to change when state changes

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,115 +1,126 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../../appContext";
 
 class Navbar extends React.Component {
-  state = {
-    isAdmin: false,
-    isLoggedIn: false
-  };
-
-  componentDidMount = () => {
-    const token = localStorage.getItem("token");
-    if (token !== null) {
-      this.setState({ isLoggedIn: true });
-      const decoded = jwtDecode(token);
-      if (decoded.is_admin === "True") {
-        this.setState({ isAdmin: true });
-      }
-    }
-  };
-  //logged in as a caterer/admin
-  // adminLogIn = () => (
-  //   <ul className="navbar-nav">
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Username
-  //       </Link>
-  //     </li>
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Meals
-  //       </Link>
-  //     </li>
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Menu
-  //       </Link>
-  //     </li>
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Orders
-  //       </Link>
-  //     </li>
-  //   </ul>
-  // );
-
-  // logged in as a customer
-  // userLogIn = () => (
-  //   <ul className="navbar-nav">
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Username
-  //       </Link>
-  //     </li>
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Notifications
-  //       </Link>
-  //     </li>
-  //     <li className="nav-item active">
-  //       <Link className="nav-link" to="/">
-  //         Order History
-  //       </Link>
-  //     </li>
-  //
-  //   </ul>
-  // );
-
   render() {
-    const { isLoggedIn } = this.state;
-
     return (
-      <nav className="navbar navbar-expand-lg navbar-light topnav">
-        <a className="navbar-brand" href="/">
-          Book A Meal
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav float-left">
-            {isLoggedIn ? (
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Log Out
-                </Link>
-              </li>
-            ) : (
-              <li className="nav-item active">
-                <Link className="nav-link" to="/login">
-                  Log In
-                </Link>
-              </li>
-            )}
-          </ul>
-        </div>
-      </nav>
+      <AppContext.Consumer>
+        {context => {
+          return (
+            <nav className="navbar navbar-expand-lg navbar-light topnav">
+              <a className="navbar-brand">Book A Meal</a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                {context.isAdmin && (
+                  <ul className="nav navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/admin"
+                      >
+                        Meals
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/adminmenu"
+                      >
+                        Menu
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/adminorders"
+                      >
+                        Orders
+                      </NavLink>
+                    </li>
+                  </ul>
+                )}
+                {context.isUser && (
+                  <ul className="nav navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/user"
+                      >
+                        Menus
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/usersorders"
+                      >
+                        Order History
+                      </NavLink>
+                    </li>
+                  </ul>
+                )}
+                {context.isLoggedIn ? (
+                  <ul className="nav navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <div className="nav-link" to="#">
+                        <i className="fa fa-user fa-fw" aria-hidden="true" />
+                        {context.email}
+                      </div>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="#logout"
+                        onClick={() => context.logout()}
+                      >
+                        Log Out
+                      </NavLink>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="nav navbar-nav">
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/"
+                      >
+                        Home
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        activeClassName="active"
+                        to="/login"
+                      >
+                        Log In
+                      </NavLink>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            </nav>
+          );
+        }}
+      </AppContext.Consumer>
     );
   }
 }
