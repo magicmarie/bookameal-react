86ffa792b79cf9be8156c90a35afaddd1e655d2d
magicmarie <natukunda162@gmail.com>
2018-08-31 08:47:15 +0300

    Implement appcontext concept

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Notifications from "react-notify-toast";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/Signup";
 import Login from "./components/Login";
@@ -9,28 +10,78 @@ import Footer from "./components/common/Footer";
 import User from "./components/User";
 import Admin from "./components/Admin";
 import AuthRoute from "./components/common/Auth";
+import AdminRoute from "./components/common/Admin";
+import GuestRoute from "./components/common/Guest";
 import AdminMenu from "./components/AdminMenu";
 import UsersOrders from "./components/UsersOrders";
+import AdminOrders from "./components/AdminOrders";
+import NotFound from "./components/NotFound";
+import AppProvider from "./appContext";
 
-const App = ({ location }) => (
-  <BrowserRouter>
-    <div>
-      <Navbar />
-      <div className="container-fluid">
-        <Notifications />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signup" exact component={SignUp} />
-          <Route path="/login" exact component={Login} />
-          <AuthRoute path="/user" exact component={User} />
-          <AuthRoute path="/admin" exact component={Admin} />
-          <AuthRoute path="/adminmenu" exact component={AdminMenu} />
-          <AuthRoute path="/usersorders" exact component={UsersOrders} />
-        </Switch>
-      </div>
-      <Footer />
-    </div>
-  </BrowserRouter>
-);
+class App extends Component {
+  render = () => {
+    return (
+      <AppProvider>
+        <Navbar />
+        <div className="container-fluid">
+          <Notifications />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <GuestRoute
+              location={this.props.location}
+              path="/signup"
+              exact
+              component={SignUp}
+            />
+            <GuestRoute
+              location={this.props.location}
+              path="/login"
+              exact
+              component={Login}
+            />
+            <AuthRoute
+              location={this.props.location}
+              path="/user"
+              exact
+              component={User}
+            />
+            <AdminRoute
+              location={this.props.location}
+              path="/admin"
+              exact
+              component={Admin}
+            />
+            <AdminRoute
+              location={this.props.location}
+              path="/adminmenu"
+              exact
+              component={AdminMenu}
+            />
+            <AuthRoute
+              location={this.props.location}
+              path="/usersorders"
+              exact
+              component={UsersOrders}
+            />
+            <AdminRoute
+              location={this.props.location}
+              path="/adminorders"
+              exact
+              component={AdminOrders}
+            />
+            <Route exact component={NotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </AppProvider>
+    );
+  };
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default App;
---
+++ src/appContext.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import jwtDecode from "jwt-decode";
+
+export const AppContext = React.createContext();
+
+class AppProvider extends Component {
+  state = {
+    isAdmin: false,
+    isLoggedIn: false,
+    isUser: false,
+    email: "",
+    logout: () => {}
+  };
+
+  logout = () => {
+    localStorage.removeItem("token");
+    this.setState({
+      isAdmin: false,
+      isLoggedIn: false,
+      isUser: false,
+      email: ""
+    });
+  };
+
+  login = token => {
+    this.handleToken(token);
+  };
+
+  handleToken = token => {
+    if (token !== null) {
+      this.setState({ isLoggedIn: true });
+      const decoded = jwtDecode(token);
+      if (decoded.is_admin === "True") {
+        this.setState({ isAdmin: true, email: decoded.email });
+      } else if (decoded.is_admin === "False") {
+        this.setState({ isUser: true, email: decoded.email });
+      }
+    }
+  };
+
+  componentDidMount = () => {
+    const token = localStorage.getItem("token");
+    this.handleToken(token);
+    this.setState({ logout: this.logout, login: this.login });
+  };
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
+
+export default AppProvider;
